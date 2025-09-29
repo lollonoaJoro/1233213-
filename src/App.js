a093fd97ad9fa033b6486ca31390cd8f7013988c
lollonoaJoro <61594883+lollonoaJoro@users.noreply.github.com>
2025-09-29 18:07:34 +0900

    과제제출

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  // 검색어: 상태(state)로 관리 -> 사용자에게 즉각적으로 UI를 보여주기 위해
+  const [searchText, setSearchText] = useState("");
+
+  // 1. input창에 접근하기 위한 useRef를 선언하세요.
+  const inputRef = useRef;
+  // 2. input창에서 검색 단어를 받아와 상태를 업데이트 하는 함수를 작성하세요.
+  const getSearchValue = (e) => {
+    setSearchText(inputRef.current.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>OZ 코딩스쿨</h2>
+      <p>검색할 키워드를 입력한 후 검색 버튼을 클릭하세요.</p>
+      <div className="search-input">
+        {/* 3. 선언한 useRef와 input을 연결하세요. */}
+        <input type="text" ref={inputRef} />
+        {/* 4. 검색 버튼을 클릭하면 검색어의 상태가 변경되도록 하세요.*/}
+        <button onClick={getSearchValue}>검색</button>
+      </div>
+      {searchText && (
+        <div>
+          🔎 <b>{searchText}</b> 에 대해 검색합니다 . . .
+        </div>
+      )}
+    </>
   );
 }
 
